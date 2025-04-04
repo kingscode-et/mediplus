9bc509de9f55d72420a0a3499cf1be35ae21886a
kingscode-et <kingscode25@gmail.com>
2025-04-04 05:17:08 -0700

    Add files via upload

---
+++ static/js/map-active.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+	/*====================================
+		Google Map JS
+	======================================*/ 	
+	(function(){
+	
+		var map;
+		map = new GMaps({
+			el: '#myMap',
+			lat: 51.507351,
+			lng: -0.127758,
+			scrollwheel: false,
+			zoom: 15,
+			zoomControl: false,
+			panControl: false,
+			streetViewControl: true,
+			mapTypeControl: false,
+			overviewMapControl: false,
+			clickable: false
+		});
+	
+		var image = 'http://localhost:8000/map-marker.png';
+	
+		// Function to locate user's device
+		GMaps.geolocate({
+			success: function(position) {
+				map.setCenter(position.coords.latitude, position.coords.longitude);
+				map.addMarker({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+					icon: image,
+					animation: google.maps.Animation.DROP,
+					verticalAlign: 'bottom',
+					horizontalAlign: 'left',
+					backgroundColor: '#efece0',
+				});
+			},
+			error: function(error) {
+				alert('Geolocation failed: ' + error.message);
+			},
+			not_supported: function() {
+				alert("Your browser does not support geolocation");
+			}
+		});
+	
+		var styles = [
+			{
+				"featureType": "road",
+				"stylers": [
+					{ "color": "#ffffff" }
+				]
+			},{
+				"featureType": "water",
+				"stylers": [
+					{ "color": "#bde5f6" }
+				]
+			},{
+				"featureType": "landscape",
+				"stylers": [
+					{ "color": "#f2f2f2" }
+				]
+			},{
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{ "color": "#FF7550" }
+				]
+			},{
+				"featureType": "poi",
+				"stylers": [
+					{ "color": "#e2f0cd" }
+				]
+			},{
+				"elementType": "labels.text",
+				"stylers": [
+					{ "saturation": 2 },
+					{ "weight": 0.3 },
+					{ "color": "#a8a8a8" }
+				]
+			}
+		];
+	
+		map.addStyle({
+			styledMapName: "Styled Map",
+			styles: styles,
+			mapTypeId: "map_style"
+		});
+	
+		map.setStyle("map_style");
+	
+	}());
+	
+	});
+	
